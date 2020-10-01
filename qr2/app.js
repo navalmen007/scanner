e032d527b0ae19f4fbee1ee0d03fea1dafbfecd4
navalmen007 <navalmen007@gmail.com>
2020-10-01 14:14:03 +0100

    chose camera{cameras.length

--- qr2/app.js
+++ qr2/app.js
@@ -49,7 +49,7 @@ scanner.addListener('scan', function (result) {
 
 Instascan.Camera.getCameras().then(function (cameras) {
         if (cameras.length > 0) {
-             CamSelected = cameras[0];
+             CamSelected = cameras[cameras.length];
              for (var i = 0, len = cameras.length; i < len; i++) {
                 if (cameras[i].name.indexOf('back') != -1) {
                         CamSelected = cameras[i];
